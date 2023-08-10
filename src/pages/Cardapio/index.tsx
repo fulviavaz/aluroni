caef088f04c99bbc17e7b8ad617ec52ad6a52ce4
fulviavaz <fulviavaz@hotmail.com>
2023-08-10 14:24:02 -0300

    create filter

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,10 +2,12 @@ import { useState } from 'react';
 import Buscador from './Buscador';
 import styles from './Cardapio.module.scss';
 import { ReactComponent as Logo } from 'assets/logo.svg';
+import Filtros from './Filtros';
 
 export default function Cardapio() {
 
   const [busca, setBusca] = useState('');
+  const [filtro, setFiltro] = useState< number | null >(null);
 
   return (
       <main>
@@ -19,6 +21,9 @@ export default function Cardapio() {
         <section className={styles.cardapio}>
           <h3 className={styles.cardapio__titulo}>Cardápio</h3>
           <Buscador busca={busca} setBusca={setBusca} />
+          <div className={styles.cardapio__filtros}>
+            <Filtros filtro={filtro} setFiltro={setFiltro} />
+          </div>
         </section>
           </nav>
     </main>
