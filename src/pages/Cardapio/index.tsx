35a358c64bf79248e3aa2f93ab48865505fe4ae3
fulviavaz <fulviavaz@hotmail.com>
2023-10-25 14:52:41 -0300

    fix container

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,4 +1,5 @@
 import styles from "./Cardapio.module.scss";
+import stylesTema from "styles/Tema.module.scss";
 
 import Buscador from "./Buscador";
 import { useState } from "react";
@@ -11,7 +12,7 @@ export default function Cardapio() {
   const [ordenador, setOrdenador] = useState("");
   return (
     <section className={styles.cardapio}>
-      <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+      <h3 className={stylesTema.titulo}>Cardápio</h3>
       <Buscador busca={busca} setBusca={setBusca} />
       <div className={styles.cardapio__filtros}>
         <Filtros filtro={filtro} setFiltro={setFiltro} />
--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,5 +1,6 @@
 import cardapio from "data/cardapio.json";
 import styles from "./Inicio.module.scss";
+import stylesTema from "styles/Tema.module.scss";
 
 export default function Inicio() {
   let pratosRecomendados = [...cardapio];
@@ -8,7 +9,7 @@ export default function Inicio() {
     .splice(0, 3);
   return (
     <section>
-      <h3 className={styles.titulo}>Recomendações da cozinha</h3>
+      <h3 className={stylesTema.titulo}>Recomendações da cozinha</h3>
       <div className={styles.recomendados}>
         {pratosRecomendados.map((item) => (
           <div key={item.id} className={styles.recomendado}>
