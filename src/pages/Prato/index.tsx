aec5b31af00ea675d213662e755b36cfea219926
fulviavaz <fulviavaz@hotmail.com>
2023-11-07 13:42:03 -0300

    create pratos

---
+++ src/pages/Prato/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./Prato.module.scss";
+import { useParams } from "react-router-dom";
+
+export default function Prato() {
+  return <div>Prato</div>;
+}
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import Sobre from "pages/Sobre";
 import Footer from "components/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "pages/NotFound";
+import Prato from "pages/Prato";
 
 export default function AppRouter() {
   return (
@@ -19,6 +20,7 @@ export default function AppRouter() {
             <Route path="sobre" element={<Sobre />} />
           </Route>
           <Route path="*" element={<NotFound />} />
+          <Route path="prato/:id" element={<Prato />} />
         </Routes>
         <Footer />
       </Router>
