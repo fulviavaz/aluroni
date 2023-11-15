547adce62fb8d903940da2d208f2492f7f0959a6
fulviavaz <fulviavaz@hotmail.com>
2023-11-15 17:02:45 -0300

    refactorating tags

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./Prato.module.scss";
 import { useParams, useNavigate } from "react-router-dom";
-import classNames from "classnames";
 import cardapio from "data/cardapio.json";
+import TagsPrato from "components/TagsPrato";
 
 export default function Prato() {
   const { id } = useParams();
@@ -23,24 +23,7 @@ export default function Prato() {
         </div>
         <div className={styles.conteudo}>
           <p className={styles.conteudo__descricao}>{prato.description}</p>
-          <div className={styles.tags}>
-            <div
-              className={classNames({
-                [styles.tags__tipo]: true,
-                [styles[`tags__tipo__${prato.category.label.toLowerCase()}`]]:
-                  true,
-              })}
-            >
-              {prato.category.label}
-            </div>
-            <div className={styles.tags__porcao}>{prato.size}g</div>
-            <div className={styles.tags__qtdpessoas}>
-              Serve {prato.serving} pessoa{prato.serving === 1 ? "" : "s"}
-            </div>
-            <div className={styles.tags__valor}>
-              R$ {prato.price.toFixed(2)}
-            </div>
-          </div>
+        <TagsPrato {...prato} />
         </div>
       </section>
     </>
