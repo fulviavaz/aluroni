9feab0f241bc7b007cc756c93fecc531dd52d7a9
fulviavaz <fulviavaz@hotmail.com>
2023-11-15 16:42:50 -0300

    create types

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import styles from "./Prato.module.scss";
-import { useLocation } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import classNames from "classnames";
 import cardapio from "data/cardapio.json";
 
 export default function Prato() {
-  const { state } = useLocation();
-  const { prato } = state as { prato: (typeof cardapio)[0] };
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const prato = cardapio.find(item => item.id === Number(id));
+  
+  if (!prato) {
+    return '';
+  }
   // console.log(useLocation);
   return (
     <>
-      <button className={styles.voltar}>{"< voltar"}</button>
+      <button onClick={() => navigate(-1)} className={styles.voltar}>{"< voltar"}</button>
       <section className={styles.container}>
         <h1 className={styles.titulo}>{prato.title}</h1>
         <div className={styles.imagem}>
